b83c8287931eb323939578ab7745ae9f6669d798
Darrick Wiebe <darrickw@gmail.com>
2025-08-26 00:00:14 -0400

    Strip more unneeded tools

--- test/serena/test_edit_marker.py
+++ test/serena/test_edit_marker.py
@@ -1,12 +1,12 @@
-from serena.tools import ListDirTool, ReplaceRegexTool, Tool
+from serena.tools import GetSymbolsOverviewTool, ReplaceRegexTool, Tool
 
 
 class TestEditMarker:
     def test_tool_can_edit_method(self):
         """Test that Tool.can_edit() method works correctly"""
         # Non-editing tool should return False
-        assert issubclass(ListDirTool, Tool)
-        assert not ListDirTool.can_edit()
+        assert issubclass(GetSymbolsOverviewTool, Tool)
+        assert not GetSymbolsOverviewTool.can_edit()
 
         # Editing tool should return True
         assert issubclass(ReplaceRegexTool, Tool)
